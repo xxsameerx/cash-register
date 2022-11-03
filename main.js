f35ba5293bcc4ab6eafef512e337b36a230a5868
xxsameerx <mdsameerabbas777@gmail.com>
2022-11-03 23:55:29 +0530

    doc

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
   if (billAmount.value > 0) {
-    // 12
+   
     if (cashGiven.value >= billAmount.value) {
-      // 2022> 12 => true
+     
       const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
       calculateChange(amountToBeReturned);
     } else {
-      showMessage("Do you wanna wash plates?");
+      showMessage("Give me more money");
     }
   } else {
     showMessage("Invalid Bill Amount");
@@ -23,18 +23,17 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 });
 
 function calculateChange(amountToBeReturned) {
-  // 2010
-  // go over all the available
+ 
   for (let i = 0; i < availableNotes.length; i++) {
-    // no of notes need for the denomination
+    
     const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    // 2010 / 2000 = 1 || 10 / 500 = 0
+  
 
-    // amount left after calculating the number of notes needed
+    
     amountToBeReturned = amountToBeReturned % availableNotes[i];
-    // 2010 % 2000 = 10 || 10 % 500 = 10
+    
 
-    // updating the no of notes in the table for the current amount
+    
     noOfNotes[i].innerText = numberOfNotes;
   }
 }
